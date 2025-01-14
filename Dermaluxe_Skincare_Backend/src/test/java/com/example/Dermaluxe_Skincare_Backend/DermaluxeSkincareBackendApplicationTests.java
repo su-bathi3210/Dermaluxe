2838c75f8550cd28578a5dce6a46d56466664cec
su-bathi3210 <ksuba3210@gmail.com>
2025-01-14 12:01:27 +0530

    backend added

---
+++ Dermaluxe_Skincare_Backend/src/test/java/com/example/Dermaluxe_Skincare_Backend/DermaluxeSkincareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dermaluxe_Skincare_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermaluxeSkincareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
