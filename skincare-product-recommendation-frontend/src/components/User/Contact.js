efe768f0f5b99a836def7b83c00f2db0956ac20d
su-bathi3210 <ksuba3210@gmail.com>
2025-02-02 15:42:27 +0530

    Updated UI/UX Design

--- skincare-product-recommendation-frontend/src/components/User/Contact.js
+++ skincare-product-recommendation-frontend/src/components/User/Contact.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import Header from '../../components/User/Header';
-import './Contact.css';
+import React from "react";
+import Header from "../../components/User/Header";
+import "./Contact.css";
 
-import contact from '../../images/contact.png';
+import Video7 from "../../images/Video7.mp4";
+
+
+import ChatIcon from "../../images/chat.png";
+import EmailIcon from "../../images/email.png";
+import PhoneIcon from "../../images/call.png";
 
 const Contact = () => {
     return (
         <div>
             <Header />
 
-            <div className="contact-image">
-                <img src={contact} alt="contact" />
+            <div className="free-video">
+                <video controls autoPlay loop muted>
+                    <source src={Video7} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
         </div>
     );
